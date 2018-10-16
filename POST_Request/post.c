99e615822552a655ed538eea45442d1083529fd0
GustavoBelfort <gmb@cesar.school>
2018-10-16 11:44:29 -0300

    Adding first working version of the post request script

--- POST_Request/post.c
+++ POST_Request/post.c
@@ -1,138 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <curl/curl.h>
 
+int main (int argc, char *argv[]) {
+    CURL *curl;
+    CURLcode res;
 
-int main(int argc, char *argv[]) {
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if (curl == NULL) {
+        return 128;
+    }
 
- char aszXmlData[]="<body><name>Gustavo</name><age>20</age></body>";
- char aszXmlRequest[250]={0};
- char aszServiceMethod[]="docs.postman-echo.com/";
- char aszRequest[150]={0};
- char aszHostIp[30]="74.125.28.121";
- char aszPort[]="80";
+    char* jsonObj = "{ \"name\" : \"Teste\" , \"idade\" : \"999\" }";
 
- sprintf(aszRequest,"http://%s:%s/%s/%s HTTP/1.1",aszHostIp,aszPort,aszServiceMethod);
- printf("Method and Resource path is below:\n\n\n");
- printf("%s",aszRequest);
+    struct curl_slist *headers = NULL;
+    curl_slist_append(headers, "Accept: application/json");
+    curl_slist_append(headers, "Content-Type: application/json");
+    curl_slist_append(headers, "charsets: utf-8");
 
-  strcat(aszHostIp,":");
-    strcat(aszHostIp,aszPort);
+    curl_easy_setopt(curl, CURLOPT_URL, "https://postman-echo.com/post");
 
- printf("\n\nHOST header is below:\n\n\n");
- printf("%s",aszHostIp);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonObj);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
 
- sprintf(aszXmlRequest,"POST %s\r\nHost: %s\r\nContent-Type: application/xml\r\nContent-Length: %d\r\n\r\n%s\r\n",aszRequest,aszHostIp,strlen(aszXmlData),aszXmlData);
+	curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, main);
+    res = curl_easy_perform(curl);
 
- printf("\n\nPOST Request which send to the server:\n\n\n");
- printf("%s",aszXmlRequest);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
 
-
- return 0;
+    return res;
 }
-
-/*
- * An example of reading JSON from stdin and printing its content to stdout.
- * The output looks like YAML, but I'm not sure if it's really compatible.
- */
-
-/*static int dump(const char *js, jsmntok_t *t, size_t count, int indent) {
-	int i, j, k;
-	if (count == 0) {
-		return 0;
-	}
-	if (t->type == JSMN_PRIMITIVE) {
-		printf("%.*s", t->end - t->start, js+t->start);
-		return 1;
-	} else if (t->type == JSMN_STRING) {
-		printf("'%.*s'", t->end - t->start, js+t->start);
-		return 1;
-	} else if (t->type == JSMN_OBJECT) {
-		printf("\n");
-		j = 0;
-		for (i = 0; i < t->size; i++) {
-			for (k = 0; k < indent; k++) printf("  ");
-			j += dump(js, t+1+j, count-j, indent+1);
-			printf(": ");
-			j += dump(js, t+1+j, count-j, indent+1);
-			printf("\n");
-		}
-		return j+1;
-	} else if (t->type == JSMN_ARRAY) {
-		j = 0;
-		printf("\n");
-		for (i = 0; i < t->size; i++) {
-			for (k = 0; k < indent-1; k++) printf("  ");
-			printf("   - ");
-			j += dump(js, t+1+j, count-j, indent+1);
-			printf("\n");
-		}
-		return j+1;
-	}
-	return 0;
-}
-
-int main() {
-	int r;
-	int eof_expected = 0;
-	char *js = NULL;
-	size_t jslen = 0;
-	char buf[BUFSIZ];
-
-	jsmn_parser p;
-	jsmntok_t *tok;
-	size_t tokcount = 2;
-
-	/* Prepare parser */
-	//jsmn_init(&p);
-
-	/* Allocate some tokens as a start */
-/*	tok = malloc(sizeof(*tok) * tokcount);
-	if (tok == NULL) {
-		fprintf(stderr, "malloc(): errno=%d\n", errno);
-		return 3;
-	}
-
-	for (;;) {
-		/* Read another chunk */
-	/*	r = fread(buf, 1, sizeof(buf), stdin);
-		if (r < 0) {
-			fprintf(stderr, "fread(): %d, errno=%d\n", r, errno);
-			return 1;
-		}
-		if (r == 0) {
-			if (eof_expected != 0) {
-				return 0;
-			} else {
-				fprintf(stderr, "fread(): unexpected EOF\n");
-				return 2;
-			}
-		}
-
-		js = realloc_it(js, jslen + r + 1);
-		if (js == NULL) {
-			return 3;
-		}
-		strncpy(js + jslen, buf, r);
-		jslen = jslen + r;
-
-again:
-		r = jsmn_parse(&p, js, jslen, tok, tokcount);
-		if (r < 0) {
-			if (r == JSMN_ERROR_NOMEM) {
-				tokcount = tokcount * 2;
-				tok = realloc_it(tok, sizeof(*tok) * tokcount);
-				if (tok == NULL) {
-					return 3;
-				}
-				goto again;
-			}
-		} else {
-			dump(js, tok, p.toknext, 0);
-			eof_expected = 1;
-		}
-	}
-
-	return EXIT_SUCCESS;
-}
-*/
